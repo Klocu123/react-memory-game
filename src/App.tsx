719451dfa30c6eda4940d2471d010103a92def4a
Klocu123 <fifipa06@gmail.com>
2024-10-28 08:24:38 +0100

    update in app.tsx adding board size changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import { CardContainer } from './containers';
 import { useMemoryCards } from './contexts/memoryContext';
 
 function App() {
   const { startGame, turn } = useMemoryCards();
+  const [boardSize, setBoardSize] = useState<string>('2x6');
   return (
     <div className='app'>
       <h1>Memory Game</h1>
+      <label htmlFor='board-size'>Select Board Size: </label>
+      <select id='board-size' value={boardSize} onChange={(e) => setBoardSize(e.target.value)}>
+        <option value='2x6'>2x6</option>
+        <option value='4x4'>4x4</option>
+        <option value='6x6'>6x6</option>
+      </select>
+      
       <button onClick={startGame}>Start a New Game</button>
       <CardContainer />
       <h2>Turn : {turn}</h2>
